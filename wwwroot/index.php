e61aeef1b9877ece6ca3833a914b1d4ea11e4dbb
root <root@af05v-VirtualBox>
2012-10-18 21:03:27 +0100

    more dev

--- wwwroot/index.php
+++ wwwroot/index.php
@@ -12,37 +12,43 @@ switch ($_SERVER['REQUEST_METHOD'])
 		break;
 }
 
+exit;
+
 
 #a file is being posted.  Institution is specified with the
 #'institution' parameter.
 function process_post()
 {
 	$institution = $_POST['institution'];
-	
+
+	if (!$institution)
+	{
+		exit_with_status(400,"You must specify and institution");
+	}
+
 	if (!valid_institution($institution))
 	{
-		#return a status code and exit
+		exit_with_status(400,"Do not recognise $institution");
 	}
 
-	$username = $_SERVER['REMOTE_USER'];
-
-	if (!valid_user($username))
+	if (!allow($institution, 'write'))
 	{
-		#return a status code and exit
-	}
-
-	if (!allow($username, $institution, 'write'))
-	{
-		#return a status code and exit
+		exit_with_status(404, "You may not post for this institution");
 	}
 
 	if (!$_FILES['file'])
 	{
-		#return a status code and exit
+		exit_with_status(400,"No file posted!");
 	}
 
 	#upload file
 	upload_posted_file($institution,$username);
+
+	#if we got here, then the upload was successful.  Any problems, and exit_with_status would have been called.
+
+
+	echo "Upload Successful";
+
 }
 
 function upload_posted_file($institution,$username)
@@ -50,37 +56,230 @@ function upload_posted_file($institution,$username)
 	global $config;
 
 	$upload_dir = $config['institutions'][$institution]['upload_dir'];
-	$tmp_file = tempnam($upload_dir,'NEW');
+	$temp_file = tempnam($upload_dir,'NEW');
 
-	$tmp_name = $_FILES["file"]["tmp_name"];
-	move_uploaded_file($tmp_name, $tmp_file);
+	move_uploaded_file($_FILES["file"]["tmp_name"], $temp_file);
 
-	if (file_is_valid($tmp_name))
+	if (file_is_valid($temp_file))
 	{
-		
+		swap_in_new_file($institution, $temp_file);
+	}
+	else
+	{
+		unlink($temp_file);
+		#We should never get here -- if there are problems, file_is_valid calls exit_with_status
+		exit_with_status(400,"Invalid CSV File");
 	}
 }
 
+#future work -- detect if the files are identical, and don't swap if they are
+function swap_in_new_file($institution, $temp_file)
+{
+	global $config;
+
+	$upload_dir = $config['institutions'][$institution]['upload_dir'];
+
+	$target_file = $upload_dir . 'active';
+
+	if (file_exists($target_file))
+	{
+		$archive_filename = $upload_dir . 'archived-on-' . date("Y-m-d-H-i-s");
+		rename($target_file, $archive_filename);
+	}
+
+	rename($temp_file, $target_file);
+}
 
 
 function file_is_valid($file_path)
 {
 	global $config;
 
+	$row_number = 1;
+	$problems = array();
+	if (($handle = fopen($file_path, "r")) !== FALSE) {
+		while (($row = fgetcsv($handle)) !== FALSE) {
+			$row_problems = array();
+			if ($row_number == 1)
+			{
+				$problems = problems_in_headings($row);
+				if ($problems)
+				{
+					break; #don't bother checking the data if the headings are bad
+				}
+			}
+			else
+			{
+				$row_problems = problems_in_data_row($row, $row_number);
+			}
+			if ($row_problems)
+			{
+				$problems = array_merge($problems, $row_problems);
+			}
+			$row_number++;
+		}
+		fclose($handle);
 
+		if ($row_number == 2) #there were no problems in the headings, but there was no data in the table
+		{
+			array_push($problems, "No Data in table");
+		}
 
+		if ($problems)
+		{
+			exit_with_status(400,"Errors in CSV File:\n\t" . implode("\n\t",$problems));
+		
+		}
+	}
+	else
+	{
+		exit_with_status(500, "Problem opening $file_path\n for reading");
+	}
+
+	return true;
 }
 
-
-#a user is attempting to read or write to a file.  Can they?
-#File names are either institution names or 'combined' for the combined file
-function allow($username, $file, $action)
+function problems_in_headings($row)
 {
 	global $config;
 
-	$permissions = $config["users"][$username]["allow_$action"];
+	$problems = array();
 
-	if (in_array($institution, $permissions) || in_array($institution, 'all'))
+	if (count($row) != count($config["csv_columns"]))
+	{
+		array_push($problems, "Incorrect number of headings in CSV");
+	}
+
+	if (!$problems)
+	{
+		for ($i = 0; $i < count($row); ++$i) {
+			if (
+				( $config["csv_columns"][$i]["validate_heading"] == 'yes' ) &&
+				( $row[$i] != $config["csv_columns"][$i]["label"] )
+			)
+			{
+				array_push($problems, "Heading " . $row[$i] . ' does not match ' . $config["csv_columns"][$i]["label"]);
+			}
+		}
+	}
+	return $problems;
+}
+
+function problems_in_data_row($row, $rowindex) 
+{
+	global $config;
+
+	$problems = array();
+
+	if (count($row) != count($config["csv_columns"]))
+	{
+		array_push($problems, "Incorrect number of elements in row $rowindex");
+	}
+
+	if (!$problems)
+	{
+		$requirement_groups;
+
+		for ($i = 0; $i < count($row); ++$i) {
+			$col = $config["csv_columns"][$i];
+			$val = $row[$i];
+
+			if (
+				$col["required"] == 'yes' &&
+				!$val
+			){
+				array_push($problems, "Row $rowindex, Column " . ($i+1) . ": Required Field Missing");
+			}
+
+			if (
+				( $col["required"] == 'one_of' )
+				
+			)
+			{
+				$requirement_groups[$col["requirement_group"]][$i+1] = $row[$i];
+			}
+
+			if ($val && $col["validate_data"])
+			{
+				$invalid = 0;
+				switch ($col["type"]){
+	#no check needed for text -- we'll accept anything
+					case 'url':
+						if (filter_var($val, FILTER_VALIDATE_URL) === false) {
+							$invalid = 1;
+						}
+						break;
+					case 'email':
+						if (filter_var($val, FILTER_VALIDATE_EMAIL) === false) {
+							$invalid = 1;
+						}
+						break;
+					case 'yes/no':
+						if (strcasecmp($val, 'yes') || strcasecmp($val, 'no'))
+						{
+							$invalid = 1;
+						}
+						break;
+					case 'wikipedia_url':
+						if (filter_var($val, FILTER_VALIDATE_URL) === false) {
+							$invalid = 1;
+						}
+						#just a quick and dirty check
+						if (!strstr($val, 'wikipedia')) {
+							$invalid = 1;
+						}
+						break;
+					case 'telephone_number':
+						#a pretty permissive regexp, allowing anything at the start, but at least 6 telephone-number-like characters at the end.
+						$opts = array( "options" => array( "regexp" => '/^.*\s*\+?[0-9 ()-]{6,}*$/' ));
+						if (filter_var($n, FILTER_VALIDATE_REGEXP, $opts ) === false) {
+							$invalid = 1;
+						}
+						break;
+				}
+				if ($invalid)
+				{
+					array_push($problems, "Row $rowindex, Column " . ($i+1) . ": Is not of type " . $col["type"]);
+				}
+			}
+		}
+
+		#at least one of the values needs to be set
+		foreach ($requirement_groups as $groupid => $values)
+		{
+			$column_ids = array();
+			$filled_flag = 0;
+			foreach ($values as $column_id => $value)
+			{
+				if ($value) { $filled_flag = 1; };
+				array_push($column_ids, $column_id);
+			}
+			if (!$filled_flag)
+			{
+				array_push($problems, "Row $rowindex, Columns " . implode(',',$column_ids) . ": At least one must be filled");
+			}
+		}
+	}
+	
+	return $problems;
+}
+
+#a user is attempting to read or write to a file.  Can they?
+#File names are either institution names or 'combined' for the combined file
+function allow($file, $action)
+{
+	global $config;
+
+	$username = $_SERVER['REMOTE_USER'];
+
+	if (!valid_user($username))
+	{
+		exit_with_status(400,"Do not recognise user $username");
+	}
+
+	$permissions = $config["users"][$username]["can_$action"];
+
+	if (in_array($institution, $permissions) || in_array('all', $permissions))
 	{
 		return true;
 	}
@@ -90,11 +289,46 @@ function allow($username, $file, $action)
 
 function process_get()
 {
+	global $config;
+
 	if (isset($_GET["file"]))
 	{
-		#check permissions and send the file
+		$filename = $_GET["file"];
+		if ($filename == 'template.csv')
+		{
+			send_template();
+		}
+		elseif ($filename == 'combined.csv')
+		{
+			if (allow('combined','read'))
+			{
+				$file_path = $config["system"]["base_path"] . $config["system"]["combined_base"] . 'active';
+				send_file($file_path, 'combined.csv');
+			}
+			else
+			{
+				exit_with_status(403,"I'm afraid I can't let you do that");
+			}
+		}
+		elseif ($filename == 'status.csv')
+		{
+			#generate status
 
-		
+		}
+		else
+		{
+			#must be an 'institution.csv' file
+			$parts = explode('.',$filename);
+			if (valid_institution($parts[0] and $parts[1] == 'csv'))
+			{
+
+				send_institution_file($parts[0]);
+			}
+			else
+			{
+				exit_with_status(404,"File parameter not recognised");
+			}
+		}
 	}
 	else
 	{
@@ -103,6 +337,68 @@ function process_get()
 	}
 }
 
+#send a non 200 status with a helpful message
+function exit_with_status($code, $message)
+{
+	header('Content-Type: text/plain; charset=utf-8');
+	header(status_code_string($code),1,$code);
+	echo $message;
+	exit;
+}
+
+function status_code_string($code)
+{
+	switch ($code){
+		case 202: return 'HTTP/1.0 202 Accepted';
+		case 400: return 'HTTP/1.0 400 Bad Request';
+		case 401: return 'HTTP/1.0 401 Unauthorized';
+		case 403: return 'HTTP/1.0 403 Forbidden';
+		case 404: return 'HTTP/1.0 404 Not Found';
+		case 500: return 'HTTP/1.0 500 Internal Server Error';
+	}
+}
+
+
+function send_template()
+{
+	global $config;
+
+	$row = array();
+
+	foreach ($config["csv_columns"] as $info)
+	{
+		array_push($row, $info['label']);
+	}
+
+	$rows = array( $row );
+
+	send_csv($rows, 'template.csv');
+}
+
+function send_csv($rows, $filename)
+{
+	// output headers so that the file is downloaded rather than displayed
+	header('Content-Type: text/csv; charset=utf-8');
+	header('Content-Disposition: attachment; filename=template.csv');
+
+	// create a file pointer connected to the output stream
+	$output = fopen('php://output', 'w');
+
+	foreach ($rows as $row)
+	{
+		fputcsv($row, $labels);
+	}
+
+	log_event('EVENT',"$filename sent");
+}
+
+function send_file($file, $filename, $mimetype = "text/csv")
+{
+	http_send_content_disposition($filename, true);
+	http_send_content_type($mimetype);
+	http_throttle(0.1, 2048);
+	http_send_file($file);
+}
 
 function log_event($event_type, $msg)
 {
@@ -117,7 +413,7 @@ function log_event($event_type, $msg)
 
 	if (!$fp)
 	{
-		die "Couldn't open $log_file for writing\n";
+		die("Couldn't open $log_file for writing\n");
 	}
 
 
@@ -125,14 +421,14 @@ function log_event($event_type, $msg)
 
 	array_push($log_parts, $event_type);
 	array_push($log_parts, date("Y-m-d H:i:s"));
-	array_push($log_parts, $_SERVER['REMOTE_HOST');
+	array_push($log_parts, $_SERVER['REMOTE_HOST']);
 	array_push($log_parts, $msg);
 
 	$log_line = implode("\t",$log_parts) . "\n";
 
 	if (!fwrite($fp, $log_line))
 	{
-		die "Couldn't write to $log_file\n";
+		die("Couldn't write to $log_file\n");
 	}
 
 	fclose($fp);
@@ -147,7 +443,7 @@ function output_query_form()
 <title>Please upload a file</title>
 </head>
 <body>
-<form method="post" action="upload.php" enctype="multipart/form-data">
+<form method="post" action="index.php" enctype="multipart/form-data">
 <label for="file">File:</label>
 <input type="file" name="file" id="file" />
 <br />
@@ -189,7 +485,7 @@ function valid_institution($institution)
 	return array_key_exists($institution, $config["institutions"]);
 }
 
-function valid_username($institution)
+function valid_user($institution)
 {
 	global $config;
 
@@ -203,19 +499,17 @@ function loadconfig($filename)
 
 	$data = file_get_contents($filename);
 
-	if ($data == NULL)
+	if (!$data)
 	{
 		$msg = "Couldn't open $filename";
-		log_event('ERROR',$msg);
 		die("$msg\n");
 	}
 
 	$decoded = json_decode($data,1);
 
-	if ($decoded == NULL)
+	if (!$decoded)
 	{
-		$msg = "Couldn't open $filename";
-		log_event('ERROR',$msg);
+		$msg = "Couldn't decode $filename";
 		die("$msg\n");
 	}
 
@@ -240,8 +534,8 @@ function create_dir_if_needed($path)
 
 	if (!mkdir($path,0770,1))
 	{
+		#don't log this event, just die.  Logging will cause recursion.
 		$msg = "couldn't create $path\n";
-		log_event('ERROR',$msg);
 		die("$msg\n");
 	}
 }
